2e5a34b0859399ca0c36446800c02540c65b8beb
SyukriHaziq <97055134+SyukriHaziq@users.noreply.github.com>
2022-11-22 15:59:18 +0800

    Add files via upload

---
+++ ShapeV2/Shape/Shape/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle aRec = new Rectangle();
+
+            Console.WriteLine("Width = {0}, Height = {1}", aRec.Width, aRec.Height);
+            aRec.Width = 4;
+            aRec.Height = 5;
+            Console.WriteLine("Width = {0}, Height = {1}", aRec.Width, aRec.Height);
+
+            Console.ReadKey();
+
+            Square aSq = new Square(8);
+
+            Console.WriteLine("Width = {0}, Height = {1}", aSq.Width, aSq.Height);
+            Console.WriteLine("Area = {0}, Circumference = {1}", aSq.Area(), aSq.Circumference());
+
+            Console.ReadKey();
+        }
+    }
+}
